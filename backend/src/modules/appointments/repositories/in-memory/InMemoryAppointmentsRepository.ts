4de2c93d9de6d9ce834aa04573785c72481c4ae5
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-08-17 19:02:01 -0300

    feat: in memory appointment repository

---
+++ backend/src/modules/appointments/repositories/in-memory/InMemoryAppointmentsRepository.ts
@@ -0,0 +1,41 @@
+import { ICreateAppointmentDTO } from '../../dtos/ICreateAppointmentDTO';
+import { Appointment } from '../../models/Appointment';
+import { IAppointmentsRepository } from '../IAppointmentsRepository';
+import { v4 as uuid } from 'uuid';
+
+class InMemoryAppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, {
+      id: uuid(),
+      date,
+      provider_id,
+    });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+
+  async findAll(): Promise<Appointment[]> {
+    const appointments = this.appointments;
+
+    return appointments;
+  }
+}
+
+export { InMemoryAppointmentsRepository };
