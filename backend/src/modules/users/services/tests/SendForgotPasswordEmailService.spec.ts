184b5f5a887cfe2be77c776880f8d274f235b68b
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-08-23 18:19:15 -0300

    test: ensure password recovery email is sent

---
+++ backend/src/modules/users/services/tests/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryMailProviderImplementations } from '../../../../shared/providers/mail/in-memory/InMemoryMailProviderImplementations';
+import { InMemoryUsersRepository } from '../../repositories/in-memory/InMemoryUsersRepository';
+import { SendForgotPasswordEmailService } from '../SendForgotPasswordEmailService';
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let inMemoryMailProvider: InMemoryMailProviderImplementations;
+let sendForgotPasswordEmail: SendForgotPasswordEmailService;
+
+describe('Send Forgot Password', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    inMemoryMailProvider = new InMemoryMailProviderImplementations();
+    sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      inMemoryUsersRepository,
+      inMemoryMailProvider,
+    );
+  });
+
+  it('should be able to send forgot password email', async () => {
+    const sendMail = jest.spyOn(inMemoryMailProvider, 'sendMail');
+
+    await inMemoryUsersRepository.create({
+      name: 'JúniorSilva',
+      email: 'junior@hotmail.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'junior@hotmail.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
